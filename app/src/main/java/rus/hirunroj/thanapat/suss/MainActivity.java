60a1eac73e65bbeb54026ba4c8dfc169574884fd
thanapat <sixmyone@gmail.com>
2016-07-13 15:44:34 +0700

    99

--- app/src/main/java/rus/hirunroj/thanapat/suss/MainActivity.java
+++ app/src/main/java/rus/hirunroj/thanapat/suss/MainActivity.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+
 
 
     @Override
