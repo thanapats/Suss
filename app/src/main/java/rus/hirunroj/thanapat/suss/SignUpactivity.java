9e1a27cffd14c42a4defbe3165e695b57783350a
thanapat <sixmyone@gmail.com>
2016-07-13 14:31:08 +0700

    update fi

--- app/src/main/java/rus/hirunroj/thanapat/suss/SignUpactivity.java
+++ app/src/main/java/rus/hirunroj/thanapat/suss/SignUpactivity.java
@@ -1,45 +1,55 @@
 package rus.hirunroj.thanapat.suss;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-public class SignUpactivity extends AppCompatActivity {
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
-    // Explicit
+import java.io.IOException;
+
+    public class SignUpactivity extends AppCompatActivity {
+
+    //Explicit
     private EditText nameEditText, userEditText, passwordEditText;
-    private RadioGroup RadioGroup;
-    private RadioButton avata0RadioButton, avata1RadioButton, avata2RadioButton, avata3RadioButton,
-            avata4RadioButton;
-
-
-    private  String nameString, userString, passwordString, avataString;
+    private RadioGroup radioGroup;
+    private RadioButton avata0RadioButton, avata1RadioButton,
+            avata2RadioButton, avata3RadioButton, avata4RadioButton;
+    private String nameString, userString, passwordString, avataString;
+    private static final String urlPHP = "http://swiftcodingthai.com/rus/add_user_master.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_upactivity);
 
-
         //Bind Widget
         nameEditText = (EditText) findViewById(R.id.editText);
         userEditText = (EditText) findViewById(R.id.editText2);
         passwordEditText = (EditText) findViewById(R.id.editText3);
-        RadioGroup = (RadioGroup) findViewById(R.id.radAvata);
-        avata0RadioButton = (RadioButton)  findViewById(R.id.radioButton);
-        avata1RadioButton = (RadioButton)  findViewById(R.id.radioButton2);
-        avata2RadioButton = (RadioButton)  findViewById(R.id.radioButton3);
-        avata3RadioButton = (RadioButton)  findViewById(R.id.radioButton4);
-        avata4RadioButton = (RadioButton)  findViewById(R.id.radioButton5);
+        radioGroup = (RadioGroup) findViewById(R.id.radAvata);
+        avata0RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avata1RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avata2RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avata3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avata4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
         //Radio Controller
-        RadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i ) {
-                switch (i){
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+
                     case R.id.radioButton:
                         avataString = "0";
                         break;
@@ -56,47 +66,83 @@ public class SignUpactivity extends AppCompatActivity {
                         avataString = "4";
                         break;
 
-                }//switch
+                }   // switch
 
             }
         });
 
-    }   //Main Method
 
-    public  void  clickSignUpSign(View view){
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
 
         //Get Value from Edit Text
         nameString = nameEditText.getText().toString().trim();
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
-    //Check Space
-        if (nameString.equals("")|| userString.equals("")||passwordString.equals("")){
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
 
-            MyAlerts myAlerts = new MyAlerts();
-            myAlerts.myDialog(this,"มีช่องว่าง","กรุณากรอกทุกช่อง");
-        }else if (checkChoose()){
-            //checked
-        }else{
-            //Un check
-            MyAlerts myAlerts = new MyAlerts();
-            myAlerts.myDialog(this,"ยังไม่เลือก Avata","กรุณาเลือก Avata ด้วยค่ะ");
+            MyAlerts myAlert = new MyAlerts();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
+
+        } else if (checkChoose()) {
+            //Checked
+            updateNewUserToServer();
+
+        } else {
+            //Un Check
+            MyAlerts myAlert = new MyAlerts();
+            myAlert.myDialog(this, "ยังไม่เลือก Avata",
+                    "กรุณาเลือก Avata ด้วยคะ");
+
         }
 
-    }   //clickSignUp
 
-    private boolean checkChoose(){
+
+    }   // clickSignUp
+
+    private void updateNewUserToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .add("Avata", avataString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+
+
+    }   // update
+
+    private boolean checkChoose() {
 
         boolean status = true;
 
-        status = avata0RadioButton.isChecked()||
-                avata1RadioButton.isChecked()||
-                avata2RadioButton.isChecked()||
-                avata3RadioButton.isChecked()||
+        status = avata0RadioButton.isChecked() ||
+                avata1RadioButton.isChecked() ||
+                avata2RadioButton.isChecked() ||
+                avata3RadioButton.isChecked() ||
                 avata4RadioButton.isChecked();
 
         return status;
-
     }
 
-}   //Main Method
+}   // Main Class
